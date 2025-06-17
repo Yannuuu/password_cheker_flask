4d08d0ae8b073f4ef92874f424606a31b1e1346d
Yanuu <yuhtukutoprak999@gmail.com>
2025-06-17 15:37:48 +0700

    3 comit

--- app.py
+++ app.py
@@ -26,5 +26,9 @@ def predict():
     
     return jsonify({'strength': strength})
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
